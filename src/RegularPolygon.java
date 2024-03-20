b2d7addffc38e046e94d1f5754abc872c8bf6b2c
Ryan <RyanABurkhardt@gmail.com>
2024-03-19 22:57:15 -0400

    Sanity Check 1

--- src/RegularPolygon.java
+++ src/RegularPolygon.java
@@ -22,13 +22,22 @@ class RegularPolygon extends SolidPolygon {//Extend Solid Polygon
 
     public RegularPolygon(Color color, int numSides, Point centerPoint, int radius) {
         super(color, numSides);//Set Color and number of vertices based off inputted parameters instead of set vertex
+        double temp_x, temp_y;//Initilize our x and y coordinate values for calculations
         int[] x_points = new int[numSides];//Create a new empty list to store x values
         int[] y_points = new int[numSides];//Create a new empty list to store y values
         for (int i=0; i<numSides; ++i) {//Run a for loop for the number of sides
-            double temp_x = centerPoint.x + radius * Math.cos(2*Math.PI*i/numSides);//Calculate the x_coordinate value using cos
-            double temp_y = centerPoint.y + radius * Math.sin(2*Math.PI*i/numSides);//Calculate the y_coordinate value using sin
+            if (numSides % 2 == 0){
+                temp_x = centerPoint.x + radius * Math.cos(2*Math.PI*i/numSides);//Calculate the x_coordinate value using cos if even number of points
+                temp_y = centerPoint.y + radius * Math.sin(2*Math.PI*i/numSides);//Calculate the y_coordinate value using sin if even number of points
+            }
+            else{
+                temp_x = centerPoint.x + radius * Math.cos((2*Math.PI*i/numSides)-Math.PI/2);//Calculate the x_coordinate value using cos if odd number of points to straighten by shifting 1 triangle to the left
+                temp_y = centerPoint.y + radius * Math.sin((2*Math.PI*i/numSides)-Math.PI/2);//Calculate the y_coordinate value using sin if odd number of points to straighten by shifting 1 triangle to the left
+            }
             x_points[i] = (int)temp_x;//Convert indexed x value Double to int and store it in the x_points array
+            //System.out.println("DEBUG: "+ temp_x);
             y_points[i] = (int)temp_y;//Convert indexed y value Double to int and store it in the y_points array
+            //System.out.println("DEBUG: " + temp_y);
         }
         createPolygon(x_points, y_points);//Lastly create the polygon using our lists
     }
